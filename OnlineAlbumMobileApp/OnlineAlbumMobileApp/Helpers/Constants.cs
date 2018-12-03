88ad9d5e1aa6f62e5b921234dd804fe326e9ebb5
Luis Beltran <luis.beltran@itcelaya.edu.mx>
2018-12-03 19:03:47 +0100

    Initial version

--- OnlineAlbumMobileApp/OnlineAlbumMobileApp/Helpers/Constants.cs
+++ OnlineAlbumMobileApp/OnlineAlbumMobileApp/Helpers/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        public readonly static string StorageAccountConnectionString = "yout-storage-account-connection-string";
+        public readonly static string StorageAccountConnectionString = "your-storage-account-connection-string";
         public readonly static string ContainerName = "images";
     }
 }
